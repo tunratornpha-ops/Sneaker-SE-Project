32befd9f61fba836af4912cced329be14bd95135
Dear <dear@MacBook-Air-khxng-Dear.local>
2026-03-09 12:02:56 +0700

    fix build error

--- frontend-SE-Project/src/data/products.ts
+++ frontend-SE-Project/src/data/products.ts
@@ -1,6 +1,4 @@
-import { useState } from "react";
-const [products, setProducts] = useState<any[]>([]);
-const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
 
 export const getProducts = async () => {
 
